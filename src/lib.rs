1c11570900ec3b59affd6da06d9f614492f11941
me2m8 <elmer.brannstrom@gmail.com>
2024-01-28 22:14:26 +0100

    Cleaned up the code a bit, and added a few more comments.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
-use dotenv::dotenv;
 use std::{
-    env,
     error::Error,
     time::{Duration, Instant},
 };
@@ -11,6 +9,8 @@ use raw_printer::*;
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::env;
+    use dotenv::dotenv;
 
     #[tokio::test]
     async fn parse_raw_printer_json() {
@@ -19,24 +19,28 @@ mod tests {
         let url = env::var("PRINTER_ADRESS").unwrap();
         let api_key = env::var("PRINTER_API_KEY").unwrap();
 
-        let mut lotta = PrinterBuilder::new(url, api_key)
-            .port(80)
-            .auto_refresh(Duration::from_secs(5))
-            .build();
+        let mut lotta = PrinterBuilder::new(url, api_key).build();
 
         let printer = lotta.get_printer_info().await.unwrap();
+
+        dbg!(printer);
     }
 }
 
+/// Builds a Printer struct with the given address and api key
+///
+/// optional parameters are port and auto_refresh
 pub struct PrinterBuilder {
-    url: String,
+    address: String,
     api_key: String,
     port: Option<u32>,
     auto_refresh: Option<Duration>,
 }
 
+/// Contains all the information about the printer
+/// as well as some helper functions to get the information.
 pub struct Printer {
-    url: String,
+    address: String,
     api_key: String,
     port: u32,
     client: reqwest::Client,
@@ -46,27 +50,53 @@ pub struct Printer {
 }
 
 impl PrinterBuilder {
-    pub fn new(url: String, api_key: String) -> Self {
+    /// Creates a new PrinterBuilder with the given address and api key
+    ///
+    /// If you want to use a different port than the default port 80, you can use the port() function
+    ///
+    /// You should set the auto refresh time with auto_refresh() to a value greater than 0 for simplicity, but if you
+    /// want to refresh the printer manually, you can leave it at None
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use prusa_link::PrinterBuilder;
+    ///
+    /// let printer_builder = PrinterBuilder::new("address".to_string(), "api_key".to_string())
+    ///    .port(80)
+    ///    .auto_refresh(Duration::from_secs(5));
+    ///
+    /// let printer = printer_builder.build();
+    /// ```
+    pub fn new(address: String, api_key: String) -> Self {
         Self {
-            url,
+            address,
             api_key,
             port: None,
             auto_refresh: None,
         }
     }
 
+    /// Use this function to set a different port than the default port 80
     pub fn port(mut self, port: u32) -> Self {
         self.port = Some(port);
         self
     }
 
+    /// Use this function to set the auto refresh time
+    /// The auto refresh time is the time after which the next call to get printer information,
+    /// such as nozzle temperature, will automatically refresh the printer information.
+    ///
+    /// If you don't want to use auto refresh, you can leave it at None.
+    /// You should set the auto refresh time to a value greater than 1 second to avoid spamming the printer with requests.
     pub fn auto_refresh(mut self, auto_refresh: Duration) -> Self {
         self.auto_refresh = Some(auto_refresh);
         self
     }
 
+    /// Builds the Printer struct
     pub fn build(self) -> Printer {
-        let url = self.url;
+        let address = self.address;
         let api_key = self.api_key;
         let port = self.port.unwrap_or(80);
         let client = reqwest::Client::new();
@@ -75,7 +105,7 @@ impl PrinterBuilder {
         let auto_refresh = self.auto_refresh;
 
         Printer {
-            url,
+            address,
             api_key,
             port,
             client,
@@ -89,7 +119,7 @@ impl PrinterBuilder {
 impl Printer {
     /// Returns the current PrusaLink version in a json format
     pub async fn get_version(&self) -> Result<String, Box<dyn Error>> {
-        let url = format!("http://{}/api/version", self.url);
+        let url = format!("http://{}:{}/api/version", self.address, self.port);
 
         let res = self
             .client
@@ -103,8 +133,9 @@ impl Printer {
         Ok(body)
     }
 
+    /// Returns a RawPrinter struct with all the information about the printer
     pub async fn get_printer_info(&mut self) -> Result<RawPrinter, Box<dyn Error>> {
-        let url = format!("http://{}:80/api/printer", self.url);
+        let url = format!("http://{}:{}/api/printer", self.address, self.port);
 
         let raw_printer_text = self
             .client
@@ -118,8 +149,9 @@ impl Printer {
         Ok(serde_json::from_str::<RawPrinter>(&raw_printer_text)?)
     }
 
+    /// Refreshes the internal printer information by sending a request to the printer.
     pub async fn refresh(&mut self) {
-        let url = format!("http://{}:80/api/printer", self.url);
+        let url = format!("http://{}:{}/api/printer", self.address, self.port);
 
         let raw_printer_text = self
             .client
@@ -135,23 +167,63 @@ impl Printer {
         self.printer = Some(serde_json::from_str::<RawPrinter>(&raw_printer_text).unwrap());
         self.last_refresh = Some(Instant::now());
     }
+
+    /// Returns the current nozzle temperature of the printer as an f32. 
+    ///
+    /// If auto refresh is enabled, the function will refresh the printer information if 
+    /// the specified time on last_refresh has passed, otherwise it will use the cached information.
+    ///
+    /// If auto refresh is disabled, the function will only refresh the printer information if
+    /// there is no cached information.
+    pub async fn get_nozzle_temp(&mut self) -> Result<f32, Box<dyn Error>> {
+        if self.last_refresh.is_none()
+            || (self.auto_refresh.is_some()
+                && self.last_refresh.unwrap().elapsed() > self.auto_refresh.unwrap())
+        {
+            self.refresh().await;
+        }
+
+        let printer = self.printer.as_ref().unwrap();
+
+        Ok(printer.get_nozzle_temp())
+    }
+
+    /// Returns the current bed temperature of the printer as an f32. 
+    ///
+    /// If auto refresh is enabled, the function will refresh the printer information if 
+    /// the specified time on last_refresh has passed, otherwise it will use the cached information.
+    ///
+    /// If auto refresh is disabled, the function will only refresh the printer information if
+    /// there is no cached information.
+    pub async fn get_bed_temp(&mut self) -> Result<f32, Box<dyn Error>> {
+        if self.last_refresh.is_none()
+            || (self.auto_refresh.is_some()
+                && self.last_refresh.unwrap().elapsed() > self.auto_refresh.unwrap())
+        {
+            self.refresh().await;
+        }
+
+        let printer = self.printer.as_ref().unwrap();
+
+        Ok(printer.get_bed_temp())
+    }
 }
 
 // impl block for minor helper functions
 impl Printer {
-    /// Returns a reference to the url string
-    pub fn url(&self) -> &String {
-        &self.url
+    /// Returns a reference to the address string
+    pub fn address(&self) -> &str {
+        &self.address
     }
 
     /// Returns a reference to the api_key string
-    pub fn api_key(&self) -> &String {
+    pub fn api_key(&self) -> &str {
         &self.api_key
     }
 
     /// Changes the APIs url
-    pub fn change_url(&mut self, url: String) {
-        self.url = url;
+    pub fn change_address(&mut self, address: String) {
+        self.address = address;
     }
 
     /// changes the APIs api key
--- src/raw_printer.rs
+++ src/raw_printer.rs
@@ -4,19 +4,19 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct PrinterTemperature {
-    /// A hashmap containing the tools and bed of the printer.
-    /// Access the bed with "bed" and the first tool with "tool0"
-    #[serde(flatten)]
-    devices: HashMap<String, PrinterTemperature>,
+    #[serde(rename = "tool0")]
+    nozzle: Temp,
+
+    bed: Temp,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Temp {
     /// The actual tmperature of the printer
-    actual: f64,
+    actual: f32,
 
     /// The target temperature of the printer
-    target: f64,
+    target: f32,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -61,14 +61,14 @@ struct PrinterFlags {
 
 #[derive(Serialize, Deserialize, Debug)]
 struct PrinterTelemetry {
-    bed_temp: f64,
-    nozzle_temp: f64,
+    bed_temp: f32,
+    nozzle_temp: f32,
     material: String,
-    z_height: f64,
-    print_speed: f64,
-    axis_x: Option<f64>,
-    axis_y: Option<f64>,
-    axis_z: Option<f64>,
+    z_height: f32,
+    print_speed: f32,
+    axis_x: Option<f32>,
+    axis_y: Option<f32>,
+    axis_z: Option<f32>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -94,4 +94,78 @@ pub struct RawPrinter {
     telemetry: PrinterTelemetry,
 
     storage: PrinterStorage,
+}
+
+impl RawPrinter {
+    pub fn get_paused(&self) -> bool {
+        self.state.flags.paused
+    }
+
+    pub fn get_operational(&self) -> bool {
+        self.state.flags.operational
+    }
+
+    pub fn get_ready(&self) -> bool {
+        self.state.flags.ready
+    }
+
+    pub fn get_sd_ready(&self) -> bool {
+        self.state.flags.sd_ready
+    }
+
+    pub fn get_error(&self) -> bool {
+        self.state.flags.error
+    }
+
+    pub fn get_closed_or_error(&self) -> bool {
+        self.state.flags.closed_or_error
+    }
+
+    pub fn get_finished(&self) -> bool {
+        self.state.flags.finished
+    }
+
+    pub fn get_prepared(&self) -> bool {
+        self.state.flags.prepared
+    }
+
+    pub fn get_link_state(&self) -> &str {
+        &self.state.flags.link_state
+    }
+
+    pub fn get_bed_temp(&self) -> f32 {
+        self.telemetry.bed_temp
+    }
+
+    pub fn get_target_bed_temp(&self) -> f32 {
+        self.temperature.bed.target
+    }
+
+    pub fn get_nozzle_temp(&self) -> f32 {
+        self.telemetry.nozzle_temp
+    }
+
+    pub fn get_target_nozzle_temp(&self) -> f32 {
+        self.temperature.nozzle.target
+    }
+
+    pub fn get_material_telemetry(&self) -> &str {
+        &self.telemetry.material
+    }
+
+    pub fn get_z_height_telemetry(&self) -> f32 {
+        self.telemetry.z_height
+    }
+
+    pub fn get_print_speed_telemetry(&self) -> f32 {
+        self.telemetry.print_speed
+    }
+
+    pub fn get_axis_x_telemetry(&self) -> Option<f32> {
+        self.telemetry.axis_x
+    }
+
+    pub fn get_axis_y_telemetry(&self) -> Option<f32> {
+        self.telemetry.axis_y
+    }
 }
